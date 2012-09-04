c7f8f058ca319db90b8f7f7b6433eeb5b8e9f84e
Mark Watts <mark.watts@hotmail.co.uk>
2012-09-04 11:10:01 +0100

    Basic program setting parsing plus tests.

---
+++ src/SolutionCop.Common.Tests/args.fs
@@ -0,0 +1,72 @@
+﻿namespace SolutionCop.Common.Tests
+
+open System
+open SolutionCop.Common
+open FsUnit.Xunit
+open Xunit
+
+module ``Given a string of command line arguments`` = 
+
+    let args = [| "-sln:'C:\MySolution\MySolution.sln'"; "-configuration:Debug"; "-platform:AnyCPU"; |]
+
+    module ``And one or more more mandatory settings are not included`` =
+
+        type ``When the argument list is parsed`` () = 
+
+            [<Fact>] member test.
+                ``Then a MissingSettingException is thrown if the path is not included`` () =
+                    let
+                        args' = args.[1..2]
+                    in
+                        (fun () -> Args.parse args' |> ignore) |> should throw typedefof<Args.MissingSettingException>
+
+            [<Fact>] member test.
+                ``Then a MissingSettingException is thrown if the configuration is not included`` () = 
+                    let 
+                        args' = [| args.[0]; args.[2] |]
+                    in
+                        (fun () -> Args.parse args' |> ignore) |> should throw typedefof<Args.MissingSettingException>
+
+            [<Fact>] member test.
+                ``Then a MissingSettingException is thrown if the platform is not included`` () = 
+                    let
+                        args' = args.[0..1]
+                    in
+                        (fun () -> Args.parse args' |> ignore) |> should throw typedefof<Args.MissingSettingException>
+
+
+    module ``And all mandatory settings are included`` = 
+
+        type ``When the argument list is parsed`` () =            
+
+            let path (settings : Args.Settings) = settings.Path
+            let configuration (settings : Args.Settings) = settings.Configuration
+            let platform (settings : Args.Settings) = settings.Platform            
+
+            [<Fact>] member test.
+                ``Then the path is parsed correctly (single quotes)`` () = 
+                    Args.parse args |> path |> should equal "C:\MySolution\MySolution.sln"
+
+            [<Fact>] member test.
+                ``Then the path is parsed correctly (double quotes)`` () =
+                    let
+                        args' = 
+                            [| "-sln:\"C:\MySolution\MySolution.sln\""; args.[1]; args.[2]; |]
+                    in
+                        Args.parse args |> path |> should equal "C:\MySolution\MySolution.sln"
+
+            [<Fact>] member test.
+                ``Then the path is parsed correctly (no quotes)`` () = 
+                    let 
+                        args' =
+                            [| "-sln:C:\MySolution\MySolution.sln"; args.[1]; args.[2]; |]
+                    in
+                        Args.parse args |> path |> should equal "C:\MySolution\MySolution.sln"
+
+            [<Fact>] member test.
+                ``Then the configuration is parsed correctly`` () =
+                    Args.parse args |> configuration |> should equal "Debug"
+
+            [<Fact>] member test.
+                ``Then the platform is parsed correctly`` () =
+                    Args.parse args |> platform |> should equal "AnyCPU"        
--- src/SolutionCop.Common/args.fs
+++ src/SolutionCop.Common/args.fs
@@ -1,15 +1,60 @@
 ﻿namespace SolutionCop.Common
 
 open System
+open SolutionCop.Common.Regex
 
 [<RequireQualifiedAccess>]
 module Args = 
 
+    ///Exception raised when a mandatory setting is not specified
+    type MissingSettingException (key : string) =
+        inherit Exception (String.Format ("The setting {0} is required.", key))
+        
+    ///Gets the setting with a given key from an argument list
+    let getSetting (key : string) (required : bool) (args : string []) = 
+
+        let findSetting (key : string) (arg : string) =
+
+            let pattern = 
+                String.Format ("^-{0}:(\\\"|'?)(.+?)(\\\"|'?)$", key)
+
+            matchAt pattern 1 arg
+
+        let arg = 
+            args
+            |> Array.tryPick (findSetting key)
+
+        match arg with
+        | Some str -> str
+        | _ -> 
+            if required then
+                raise (MissingSettingException (key))
+            else
+                String.Empty
+
+    ///Record describing basic program settings
     type Settings = {
         Path : String;
         Configuration : String;
         Platform : String;        
     }
+    with
 
+        static member Empty = 
+            {
+                Path = "";
+                Configuration = "";
+                Platform = "";
+            }
+
+    ///Parses program settings from the given argument list
     let parse (args : string []) = 
-        { Path = ""; Configuration = ""; Platform = ""; }
+
+        let get key = 
+            getSetting key true args
+
+        {
+            Path = (get "sln");
+            Configuration = (get "configuration");
+            Platform = (get "platform");
+        }
