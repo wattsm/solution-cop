c4c3f6a600422af21f9b26f35b6e865d41fc0c9d
Mark Watts <mark.watts@hotmail.co.uk>
2012-09-07 15:00:30 +0100

    Added function to load a VS solution from disk.

--- src/SolutionCop.VisualStudio/solution.fs
+++ src/SolutionCop.VisualStudio/solution.fs
@@ -2,16 +2,27 @@
 
 open System
 open SolutionCop.Common
+open SolutionCop.Common.IO
 
 [<RequireQualifiedAccess>]
 module Solution = 
 
+    ///Exception describing an invalid solution file
+    type InvalidSolutionException () =
+        inherit Exception ()
+
+    ///A record containing relevant information about a VS solution
     type Data = {
         Directory : String;
         FileName : String;
         Projects : Project.Data list;
     }
 
-    let load settings = 
+    ///Reads a VS solution from the given solution file contents
+    let read str = 
         { Directory = ""; FileName = ""; Projects = []; }
-        
+
+    ///Loads the VS solution identified in the given settings
+    let load (settings : Args.Settings) = 
+        contentsOf settings.Path
+        |> read
